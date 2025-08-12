fa5ccecafa1b91893536a5452f1817810cc137fb
kuszyp <piotr@myapp.pl>
2025-08-12 08:28:21 +0200

    chore: add mapMulti() example with flatten one-to-many relation

--- functional-programming/src/main/java/pl/myapp/java/examples/functional/streamApi/MapMultiExample.java
+++ functional-programming/src/main/java/pl/myapp/java/examples/functional/streamApi/MapMultiExample.java
@@ -1,14 +1,15 @@
 package pl.myapp.java.examples.functional.streamApi;
 
+import java.time.LocalDate;
 import java.util.List;
 
-/*
- * The goal is to filter even integers and double their value.
- * The classic approach uses filter and map intermediate methods.
- * The new approach uses mapMulti to achieve the same result in a more concise way.
- */
 public final class MapMultiExample {
 
+    /*
+     * The goal is to filter even integers and double their value.
+     * The classic approach uses filter and map intermediate methods.
+     * The new approach uses mapMulti to achieve the same result in a more concise way.
+     */
     List<Integer> evenDoubledNumbersClassic(List<Integer> numbers) {
         return numbers.stream()
                 .filter(number -> number % 2 == 0)
@@ -26,4 +27,31 @@ public final class MapMultiExample {
                 .toList();
     }
 
+    /*
+     * The mapMulti is pretty similar to the well-known flatMap(), which is useful to flatten a nested `Stream<Stream<R>>` model.
+     * In the example below, we have Book record and Author record. There is a relation one-to-many between Author and Book.
+     * The goal is to convert above one-to-many model to the flat one represented by a Bookshelf record.
+     */
+    record Book(String title, LocalDate published) { }
+    record Author(String name, List<Book> books) { }
+    record Bookshelf(String author, String book) { }
+
+    List<Bookshelf> bookshelfClassic(List<Author> authors) {
+        return authors.stream()
+                .flatMap(author -> author.books().stream()
+                        .map(book -> new Bookshelf(
+                                author.name(),
+                                book.title()))
+                ).toList();
+
+    }
+
+    List<Bookshelf> bookshelfNew(List<Author> authors) {
+        return authors.stream()
+                .<Bookshelf>mapMulti((author, consumer) -> {
+                    for (Book book : author.books()) {
+                        consumer.accept(new Bookshelf(author.name(), book.title()));
+                    }
+                }).toList();
+    }
 }
--- functional-programming/src/test/groovy/pl/myapp/java/examples/functional/streamApi/MapMultiExampleSpec.groovy
+++ functional-programming/src/test/groovy/pl/myapp/java/examples/functional/streamApi/MapMultiExampleSpec.groovy
@@ -3,6 +3,8 @@ package pl.myapp.java.examples.functional.streamApi
 import spock.lang.Specification
 import spock.lang.Subject
 
+import java.time.LocalDate
+
 class MapMultiExampleSpec extends Specification {
 
     @Subject
@@ -24,5 +26,36 @@ class MapMultiExampleSpec extends Specification {
 
         then:
         result2 == expected
+    }
+
+    def "should flatten Author - Book one-to-many relationship and return list of Bookshelf"() {
+        given:
+        def book1 = new MapMultiExample.Book('First Book Title', LocalDate.parse('2023-04-17'))
+        def book2 = new MapMultiExample.Book('Second Book Title', LocalDate.parse('2011-10-25'))
+        def book3 = new MapMultiExample.Book('Third Book Title', LocalDate.parse('2015-01-01'))
+        def book4 = new MapMultiExample.Book('Fourth Book Title', LocalDate.parse('2020-12-31'))
+        def author1 = new MapMultiExample.Author('Author One', [book1, book2])
+        def author2 = new MapMultiExample.Author('Author Two', [book3])
+        def author3 = new MapMultiExample.Author('Author Three', [book4])
+
+        when:
+        def result = mapMultiExample.bookshelfClassic([author1, author2, author3])
+
+        then:
+        result.size() == 4
+        result[0].author == 'Author One'
+        result[0].book == 'First Book Title'
+        result[1].author == 'Author One'
+        result[1].book == 'Second Book Title'
+        result[2].author == 'Author Two'
+        result[2].book == 'Third Book Title'
+        result[3].author == 'Author Three'
+        result[3].book == 'Fourth Book Title'
+
+        when:
+        def result2 = mapMultiExample.bookshelfNew([author1, author2, author3])
+
+        then:
+        result == result2
     }
 }
